eb12786260720cf15a906f05ec9adcddb5784661
Marcin WIelgus <mwielgus@outlook.com>
2017-11-25 05:05:44 +0100

    + premature initialization of scalike crushed other tests

--- akka-service-commons/src/test/scala/org/s4s0l/betelgeuse/akkacommons/persistence/utils/BetelgeuseDbTest.scala
+++ akka-service-commons/src/test/scala/org/s4s0l/betelgeuse/akkacommons/persistence/utils/BetelgeuseDbTest.scala
@@ -1,17 +1,17 @@
 /*
  * Copyright© 2017 the original author or authors.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 
@@ -26,11 +26,10 @@ import org.scalatest.{BeforeAndAfterAll, FeatureSpec}
   */
 class BetelgeuseDbTest extends FeatureSpec with BeforeAndAfterAll {
 
-  val scalike = new BetelgeuseDb(ConfigFactory.load("BetelgeuseDbTest.conf"))
+  lazy val scalike = new BetelgeuseDb(ConfigFactory.load("BetelgeuseDbTest.conf"))
 
   feature("BetelgeuseDb allows use of dcalikeJdbc") {
     import scalikejdbc._
-    scalike.setupAll()
 
     scenario("Readonly interpolation queries") {
       val memberIds = scalike.readOnly { implicit session =>
@@ -54,6 +53,11 @@ class BetelgeuseDbTest extends FeatureSpec with BeforeAndAfterAll {
 
       assert(tablesInSchema.size == 1)
     }
+  }
+
+  override protected def beforeAll(): Unit = {
+    scalike.setupAll()
+
   }
 
   override protected def afterAll(): Unit = {
