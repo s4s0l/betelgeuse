de8e37c3abb0f1f648b10002979a62ef6220aef2
Marcin Wielgus <mwielgus@outlook.com>
2018-01-24 19:02:44 +0100

    + configurable overflow strategy for stomp reverse actor source

--- akka-service-commons/src/main/scala/org/s4s0l/betelgeuse/akkacommons/http/stomp/StompHandler.scala
+++ akka-service-commons/src/main/scala/org/s4s0l/betelgeuse/akkacommons/http/stomp/StompHandler.scala
@@ -1,17 +1,17 @@
 /*
- * Copyright© 2017 the original author or authors.
+ * Copyright© 2018 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 
@@ -67,7 +67,9 @@ object StompHandler extends StompHandler {
     */
   final case class StompHandlerSettings(webSocketName: String, pubSubActorRef: Option[ActorRef] = None,
                                         registerAsNamed: Boolean = false,
-                                        stompServerMessageActorMapping: SocksSessionId => PartialFunction[Any, StompServerMessage] = _ => PartialFunction.empty
+                                        stompServerMessageActorMapping: SocksSessionId => PartialFunction[Any, StompServerMessage] = _ => PartialFunction.empty,
+                                        fromServerActorBufferSize: Int = 1000,
+                                        fromServerActorOverflowStrategy: OverflowStrategy = OverflowStrategy.dropHead
                                        )
 
 
@@ -334,7 +336,7 @@ trait StompHandler {
 
     val stompUserActorRef = system.actorOf(StompUserActor.props(socksSessionId, settings))
 
-    val stompMessageReceiverActor = Source.actorRef[StompMessage](bufferSize = 30, OverflowStrategy.dropHead)
+    val stompMessageReceiverActor = Source.actorRef[StompMessage](settings.fromServerActorBufferSize, settings.fromServerActorOverflowStrategy)
 
     val env = StompHandlerEnvironment(stompUserActorRef, socksSessionId)
     val xxx = flowFactory(env)
