5e8150d8f93cc0202be511d5853eef240d7bb417
Marcin Wielgus <mwielgus@outlook.com>
2018-05-29 09:00:14 +0200

    Failing tests

--- akka-service-commons/src/test/scala/org/s4s0l/betelgeuse/akkacommons/persistence/roach/RoachAsyncWriteJournalDaoTest.scala
+++ akka-service-commons/src/test/scala/org/s4s0l/betelgeuse/akkacommons/persistence/roach/RoachAsyncWriteJournalDaoTest.scala
@@ -136,8 +136,8 @@ class RoachAsyncWriteJournalDaoTest
         assert(entity.tag == "tag9")
         assert(entity.id == "123")
         assert(entity.seq == 1)
-        assert(entity.eventClass == "org.s4s0l.betelgeuse.akkacommons.serialization.JsonAnyWrapper")
-        assert(entity.event == """{"wrapped":{"stringValue":"a string value"},"@class":"org.s4s0l.betelgeuse.akkacommons.serialization.JsonAnyWrapper$StringWrapper"}""")
+        assert(entity.eventClass == "org.s4s0l.betelgeuse.akkacommons.serialization.JsonSimpleTypeWrapper")
+        assert(entity.event == """{"string":"a string value"}""")
         assert(entity.writerUuid == "writerOne")
 
         assert(!entity.deleted)
@@ -166,7 +166,7 @@ class RoachAsyncWriteJournalDaoTest
         assert(replayedEntities.head.id == "123")
         assert(replayedEntities.head.seq == 1)
         assert(replayedEntities.head.eventClass == entity.eventClass)
-        assert(replayedEntities.head.event == """{"@class": "org.s4s0l.betelgeuse.akkacommons.serialization.JsonAnyWrapper$StringWrapper", "wrapped": {"stringValue": "a string value"}}""")
+        assert(replayedEntities.head.event == """{"string": "a string value"}""")
         assert(replayedEntities.head.writerUuid == entity.writerUuid)
         assert(replayedEntities.head.deleted == entity.deleted)
         assert(replayedEntities.head.manifest == entity.manifest)
@@ -188,7 +188,6 @@ class RoachAsyncWriteJournalDaoTest
         Given("Some regular event")
         val event = CrateEvent("s", 1, Seq("a"))
         When("Entity is created")
-        val serializer = new JacksonJsonSerializer()
         intercept[ClassCastException](RoachAsyncWriteJournal.createEntity(PersistentRepr.apply(
           payload = event, sequenceNr = 1, persistenceId = "tag3/123",
           deleted = false,
@@ -225,13 +224,25 @@ class RoachAsyncWriteJournalDaoTest
             replayedMessagesAfterCleaning += e
         }
 
-        Then("Only last message is returned")
-        assert(replayedMessagesAfterCleaning.lengthCompare(1) == 0)
+        Then("No messages are returned")
+        assert(replayedMessagesAfterCleaning.lengthCompare(0) == 0)
+
+
+        When("next message is inserted")
+        setup3Events(dao, "5", dao.getMaxSequenceNumber("tag", "5", -1)+1)
+
+
+        Then("It has sequence number not lost")
+        dao.replayMessages("tag", "5", -1, 100, 100) {
+          e =>
+            replayedMessagesAfterCleaning += e
+        }
+        assert(replayedMessagesAfterCleaning.size == 3)
         assert(replayedMessagesAfterCleaning.head.tag == "tag")
         assert(replayedMessagesAfterCleaning.head.id == "5")
-        assert(replayedMessagesAfterCleaning.head.seq == 3)
+        assert(replayedMessagesAfterCleaning.head.seq == 4)
         assert(replayedMessagesAfterCleaning.head.eventClass == classOf[RegularEvent].getName)
-        assert(replayedMessagesAfterCleaning.head.event == """{"i": 3, "s": "s", "seq": ["c"]}""")
+        assert(replayedMessagesAfterCleaning.head.event == """{"i": 1, "s": "s", "seq": ["a"]}""")
         assert(replayedMessagesAfterCleaning.head.writerUuid == "writerOne")
         assert(!replayedMessagesAfterCleaning.head.deleted)
         assert(replayedMessagesAfterCleaning.head.manifest == "manifa")
@@ -239,7 +250,7 @@ class RoachAsyncWriteJournalDaoTest
 
     }
 
-    scenario("Deleting upTo sequence number > max throws") {
+    scenario("Deleting upTo sequence number > max does not throw") {
       localTx { implicit session =>
         Given("Crate async writer dao with no serializer")
 
@@ -248,8 +259,7 @@ class RoachAsyncWriteJournalDaoTest
         setup3Events(dao, "4")
 
         When("We delete messages up to sequence higher then max (3)")
-        Then("It throws")
-        assertThrows[Exception](dao.deleteUpTo("tag", "4", 4))
+        dao.deleteUpTo("tag", "4", 4)
 
         When("We check the state of events")
         val replayedMessagesAfterCleaning = mutable.Buffer[RoachAsyncWriteJournalEntity]()
@@ -259,20 +269,21 @@ class RoachAsyncWriteJournalDaoTest
             replayedMessagesAfterCleaning += e
         }
 
-        Then("None were deleted")
-        assert(replayedMessagesAfterCleaning.lengthCompare(3) == 0)
+        Then("All were deleted")
+        assert(replayedMessagesAfterCleaning.lengthCompare(0) == 0)
 
       }
     }
   }
 
-  def setup3Events(dao: RoachAsyncWriteJournalDao, id: String)(implicit session: DBSession): Unit = {
+  def setup3Events(dao: RoachAsyncWriteJournalDao, id: String, seqStart:Long = 1)
+                  (implicit session: DBSession): Unit = {
     val event1 = RegularEvent("s", 1, Seq("a"))
     val event2 = RegularEvent("s", 2, Seq("b"))
     val event3 = RegularEvent("s", 3, Seq("c"))
     When("Entities are created")
     val entity1 = RoachAsyncWriteJournal.createEntity(PersistentRepr.apply(
-      payload = event1, sequenceNr = 1, persistenceId = s"tag/$id",
+      payload = event1, sequenceNr = seqStart, persistenceId = s"tag/$id",
       deleted = false,
       manifest = "manifa",
       sender = ActorRef.noSender,
@@ -280,7 +291,7 @@ class RoachAsyncWriteJournalDaoTest
     ))
 
     val entity2 = RoachAsyncWriteJournal.createEntity(PersistentRepr.apply(
-      payload = event2, sequenceNr = 2, persistenceId = s"tag/$id",
+      payload = event2, sequenceNr = seqStart+1, persistenceId = s"tag/$id",
       deleted = false,
       manifest = "manifa",
       sender = ActorRef.noSender,
@@ -288,7 +299,7 @@ class RoachAsyncWriteJournalDaoTest
     ))
 
     val entity3 = RoachAsyncWriteJournal.createEntity(PersistentRepr.apply(
-      payload = event3, sequenceNr = 3, persistenceId = s"tag/$id",
+      payload = event3, sequenceNr = seqStart + 2, persistenceId = s"tag/$id",
       deleted = false,
       manifest = "manifa",
       sender = ActorRef.noSender,
@@ -301,7 +312,7 @@ class RoachAsyncWriteJournalDaoTest
     dao.save(immutable.Seq(entity3))
 
     Then("Max seq returns last inserted value")
-    assert(dao.getMaxSequenceNumber("tag", id, -1) == 3)
+    assert(dao.getMaxSequenceNumber("tag", id, -1) == 2 + seqStart)
 
 
     When("Replaying those events")
@@ -317,7 +328,7 @@ class RoachAsyncWriteJournalDaoTest
     assert(replayedMessages.lengthCompare(3) == 0)
     assert(replayedMessages.last.tag == "tag")
     assert(replayedMessages.last.id == id)
-    assert(replayedMessages.last.seq == 3)
+    assert(replayedMessages.last.seq == 2 + seqStart)
     assert(replayedMessages.last.eventClass == classOf[RegularEvent].getName)
     assert(replayedMessages.last.event == """{"i": 3, "s": "s", "seq": ["c"]}""")
     assert(replayedMessages.last.writerUuid == "writerOne")
