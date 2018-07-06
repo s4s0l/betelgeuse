fa79e41b0c85becfdb1342409546f06d0202aa96
Marcin Wielgus <mwielgus@outlook.com>
2018-07-06 17:48:50 +0200

    default stream in merge hub

--- akka-service-commons/src/main/scala/org/s4s0l/betelgeuse/akkacommons/streams/SequenceMergeHub.scala
+++ akka-service-commons/src/main/scala/org/s4s0l/betelgeuse/akkacommons/streams/SequenceMergeHub.scala
@@ -1,4 +1,10 @@
 /*
+ * Copyright© 2018 by Ravenetics Sp. z o.o. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * This file is proprietary and confidential.
+ */
+
+/*
  * Copyright© 2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,21 +30,42 @@ import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future, Promise}
 
 /**
+  * @param noStreamFactory - when defined will be used to create a stream every time
+  *                        no source stream is available in the hub
+  * @param killSwitch      - kill switch to source that was returned
   * @author Marcin Wielgus
   */
-class SequenceMergeHub[T, M](mergeSink: Sink[T, NotUsed], killSwitch: UniqueKillSwitch)
+class SequenceMergeHub[T, M](mergeSink: Sink[T, NotUsed],
+                             killSwitch: UniqueKillSwitch,
+                             noStreamFactory: Option[() => Source[T, M]])
                             (implicit ec: ExecutionContext,
                              materializer: Materializer) {
 
   private val sourceQueue = mutable.Queue[(Source[T, M], Promise[M])]()
   @volatile private var running: Option[(Source[T, M], M, UniqueKillSwitch)] = None
   @volatile private var down: Boolean = false
+  private var noSourceStreamKillSwitch: Option[UniqueKillSwitch] = None
+
 
   def killSource(): Unit = {
     synchronized {
       down = true
       sourceQueue.clear()
       killSwitch.shutdown()
+    }
+  }
+
+  /**
+    * plays default stream only id nothing is running
+    *
+    * @return true if default was started
+    */
+  def playDefault(): Boolean = synchronized {
+    if (running.isEmpty && noSourceStreamKillSwitch.isEmpty) {
+      runNoSourceStream()
+      true
+    } else {
+      false
     }
   }
 
@@ -57,9 +84,11 @@ class SequenceMergeHub[T, M](mergeSink: Sink[T, NotUsed], killSwitch: UniqueKill
       if (down) {
         throw new IllegalStateException("Source is down!")
       }
+      val noSourceWasRunning = noSourceStreamKillSwitch.isDefined
+      noSourceStreamKillSwitch.foreach(_.shutdown())
       val promise = Promise[M]()
       sourceQueue.enqueue((src, promise))
-      if (running.isEmpty)
+      if (running.isEmpty && !noSourceWasRunning)
         runNext()
       promise.future
     }
@@ -71,6 +100,10 @@ class SequenceMergeHub[T, M](mergeSink: Sink[T, NotUsed], killSwitch: UniqueKill
     */
   def runSource(src: Source[T, M]): Future[M] = {
     synchronized {
+      if (down) {
+        throw new IllegalStateException("Source is down!")
+      }
+      noSourceStreamKillSwitch.foreach(_.shutdown())
       sourceQueue.clear()
       running.foreach(_._3.shutdown())
       addSource(src)
@@ -86,31 +119,75 @@ class SequenceMergeHub[T, M](mergeSink: Sink[T, NotUsed], killSwitch: UniqueKill
   }
 
   private def runNext(): Unit = synchronized {
-    val tmp = dequeue()
-      .map { it =>
-        val ((x, done), ksw) = it._1
-          .watchTermination()(Keep.both)
-          .viaMat(KillSwitches.single)(Keep.both)
-          .to(mergeSink)
-          .run()
-        val ret = (it._1, it._2, x, ksw)
-        done.onComplete(_ => {
-          runNext()
-        })
-        ret
+    if (!down) {
+      val tmp: Option[(Source[T, M], Promise[M], M, UniqueKillSwitch)] = dequeue()
+        .map { it =>
+          val ((x, done), ksw) = it._1
+            .watchTermination()(Keep.both)
+            .viaMat(KillSwitches.single)(Keep.both)
+            .to(mergeSink)
+            .run()
+          done.onComplete(_ => {
+            onCompleteNormal()
+          })
+          (it._1, it._2, x, ksw)
+        }
+      running = tmp.map(it => (it._1, it._3, it._4))
+      tmp.foreach { it => it._2.success(it._3) }
+      if (running.isEmpty) {
+        runNoSourceStream()
       }
-    running = tmp.map(it => (it._1, it._3, it._4))
-    tmp.foreach { it => it._2.success(it._3) }
+    }
+  }
+
+  private def runNoSourceStream(): Unit = {
+    noStreamFactory.foreach { f =>
+      val noSourceStream = f.apply()
+      val ((_, done), ksw) = noSourceStream
+        .watchTermination()(Keep.both)
+        .viaMat(KillSwitches.single)(Keep.both)
+        .to(mergeSink)
+        .run()
+      noSourceStreamKillSwitch = Some(ksw)
+      done.onComplete(_ => {
+        onCompleteNoSourceStream()
+      })
+    }
+  }
+
+  private def onCompleteNormal(): Unit = synchronized {
+    running = None
+    runNext()
+  }
+
+  private def onCompleteNoSourceStream(): Unit = synchronized {
+    noSourceStreamKillSwitch = None
+    runNext()
   }
 
 }
 
 object SequenceMergeHub {
-  def defineSource[T, M](perProducerBufferSize: Int = 16)
+  /**
+    *
+    * @param perProducerBufferSize - see [[MergeHub]] docs
+    * @param noStreamFactory       - when defined will be used to create a stream every time
+    *                              no source stream is available in the hub, DO NOT USE FINITE
+    *                              STREAMS OF FINITE SIZE <= perProducerBufferSize, otherwise
+    *                              they will flood MERGE hub and it WILL (NOT MAY IT WILL!!)
+    *                              starve real streams. Generally it is best to use infinite streams.
+    *                              It's because of how [[MergeHub]] works, to remove
+    *                              this behaviour we would need to reimplement whole
+    *                              [[MergeHub]] or provide some look ahead tooling to see
+    *                              what was pulled from the hub really.
+    * @return
+    */
+  def defineSource[T, M](perProducerBufferSize: Int = 16,
+                         noStreamFactory: Option[() => Source[T, M]] = None)
                         (implicit ec: ExecutionContext,
                          materializer: Materializer)
   : Source[T, SequenceMergeHub[T, M]] =
     MergeHub.source[T](perProducerBufferSize)
       .viaMat(KillSwitches.single)(Keep.both)
-      .mapMaterializedValue(it => new SequenceMergeHub[T, M](it._1, it._2))
+      .mapMaterializedValue(it => new SequenceMergeHub[T, M](it._1, it._2, noStreamFactory))
 }
--- akka-service-commons/src/test/scala/org/s4s0l/betelgeuse/akkacommons/streams/SequenceMergeHubTest.scala
+++ akka-service-commons/src/test/scala/org/s4s0l/betelgeuse/akkacommons/streams/SequenceMergeHubTest.scala
@@ -1,4 +1,10 @@
 /*
+ * Copyright© 2018 by Ravenetics Sp. z o.o. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * This file is proprietary and confidential.
+ */
+
+/*
  * Copyright© 2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -39,6 +45,7 @@ class SequenceMergeHubTest
   implicit val mat: ActorMaterializer = ActorMaterializer()
   implicit val ec: ExecutionContextExecutor = system.dispatcher
   feature("Can sequence all streams dynamic") {
+
     scenario("simple test") {
       val source = SequenceMergeHub.defineSource[Int, NotUsed](1)
       val sink = TestSink.probe[Int]
@@ -49,14 +56,14 @@ class SequenceMergeHubTest
       eventualSeq.request(1)
       eventualSeq.expectNext(1)
       whenReady(added) { _ =>
-        assert(hub.currentlyRunning() == Some((source1, NotUsed.getInstance())))
+        assert(hub.currentlyRunning().contains((source1, NotUsed.getInstance())))
       }
 
       eventualSeq.request(2).expectNext(2, 3)
       val source2 = hub.addSource(Source(List()))
       eventualSeq.request(2).expectNoMessage(1.second)
       whenReady(source2) { _ =>
-        assert(hub.currentlyRunning() != Some((source1, NotUsed.getInstance())))
+        assert(!hub.currentlyRunning().contains((source1, NotUsed.getInstance())))
       }
       hub.addSource(Source(List(4, 5)))
       eventualSeq.expectNext(4)
@@ -86,6 +93,241 @@ class SequenceMergeHubTest
       eventualSeq.request(1)
       eventualSeq.expectNext(4)
     }
+  }
+
+  feature("Will create default source when no source available in the hub") {
+    scenario("simple test") {
+      val source = SequenceMergeHub
+        .defineSource[Int, NotUsed](1, Some(() => Source.repeat(42)))
+      val sink = TestSink.probe[Int]
+
+      val (hub, eventualSeq) = source.toMat(sink)(Keep.both).run()
+
+      val source1 = Source(List(1, 2, 3))
+      hub.addSource(source1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(2)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(3)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+
+      hub.killSource()
+      eventualSeq.request(1)
+      eventualSeq.expectComplete()
+    }
+
+
+    scenario("playing default on demand") {
+      val source = SequenceMergeHub
+        .defineSource[Int, NotUsed](1, Some(() => Source.repeat(42)))
+      val sink = TestSink.probe[Int]
+
+      val (hub, eventualSeq) = source.toMat(sink)(Keep.both).run()
+
+      eventualSeq.request(1)
+      eventualSeq.expectNoMessage(1.second)
+      hub.playDefault()
+      eventualSeq.expectNext(42)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+      Thread.sleep(500) //merge hub is async
+      //so we wait here to be sure buffer is populated
+      //so the test is deterministic
+      val source1 = Source(List(1, 2, 3))
+      hub.addSource(source1)
+
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(2)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(3)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+
+      hub.killSource()
+      eventualSeq.request(1)
+      eventualSeq.expectComplete()
+    }
+
+    scenario("removing default when adding new source") {
+      val source = SequenceMergeHub
+        .defineSource[Int, NotUsed](1, Some(() => Source.repeat(42)))
+      val sink = TestSink.probe[Int]
+
+      val (hub, eventualSeq) = source.toMat(sink)(Keep.both).run()
+
+      val source1 = Source(List(1, 2, 3))
+      hub.addSource(source1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(2)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(3)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+
+      Thread.sleep(500) //merge hub is async
+      //so we wait here to be sure buffer is populated
+      //so the test is deterministic
+
+      val source2 = Source(List(4, 5))
+      hub.addSource(source2)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(4)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(5)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+
+
+      hub.killSource()
+      eventualSeq.request(1)
+      eventualSeq.expectComplete()
+    }
+
+
+    scenario("removing default when running new source") {
+      val source = SequenceMergeHub
+        .defineSource[Int, NotUsed](1, Some(() => Source.repeat(42)))
+      val sink = TestSink.probe[Int]
+
+      val (hub, eventualSeq) = source.toMat(sink)(Keep.both).run()
+
+      val source1 = Source(List(1, 2, 3))
+      hub.addSource(source1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(2)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(3)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+
+      Thread.sleep(500) //merge hub is async
+      //so we wait here to be sure buffer is populated
+      //so the test is deterministic
+
+      val source2 = Source(List(4, 5))
+      hub.runSource(source2)
+
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+
+      eventualSeq.request(1)
+      eventualSeq.expectNext(4)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(5)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+
+
+      hub.killSource()
+      eventualSeq.request(1)
+      eventualSeq.expectComplete()
+    }
+
+    scenario("default does not interfer when new streams are available") {
+      val source = SequenceMergeHub
+        .defineSource[Int, NotUsed](1, Some(() => Source.repeat(42)))
+      val sink = TestSink.probe[Int]
+
+      val (hub, eventualSeq) = source.toMat(sink)(Keep.both).run()
+
+      val source1 = Source(List(1, 2, 3))
+      hub.addSource(source1)
+      val source2 = Source(List(4, 5))
+      hub.addSource(source2)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(2)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(3)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(4)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(5)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(42)
+
+
+      hub.killSource()
+      eventualSeq.request(1)
+      eventualSeq.expectComplete()
+    }
+
+
+    scenario("default is repeated when is finite") {
+      var count = 99
+      val source = SequenceMergeHub
+        .defineSource[Int, NotUsed](1, Some(() => {
+        count = count + 1
+        Source(List(count, count))
+      }))
+      val sink = TestSink.probe[Int]
+
+      val (hub, eventualSeq) = source.toMat(sink)(Keep.both).run()
+
+      val source1 = Source(List(1, 2, 3))
+      hub.addSource(source1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(1)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(2)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(3)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(100)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(100)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(101)
+      Thread.sleep(500) //merge hub is async
+      //so we wait here to be sure buffer is populated
+      //so the test is deterministic
+      val source2 = Source(List(4, 5))
+      hub.addSource(source2)
+
+      eventualSeq.request(1)
+      eventualSeq.expectNext(101)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(102)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(4)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(5)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(103)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(103)
+      eventualSeq.request(1)
+      eventualSeq.expectNext(104)
+
+
+      hub.killSource()
+      eventualSeq.request(1)
+      eventualSeq.expectComplete()
+    }
+
+
   }
 
 }
