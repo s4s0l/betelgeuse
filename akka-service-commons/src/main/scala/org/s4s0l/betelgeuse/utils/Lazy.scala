16e1da817c05b2e50127f1a05faad435d69ae874
Marcin Wielgus <mwielgus@outlook.com>
2018-08-28 16:43:28 +0200

    cleanup

--- akka-service-commons/src/main/scala/org/s4s0l/betelgeuse/utils/Lazy.scala
+++ akka-service-commons/src/main/scala/org/s4s0l/betelgeuse/utils/Lazy.scala
@@ -1,8 +1,3 @@
-/*
- * Copyright© 2018 by Ravenetics Sp. z o.o. - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * This file is proprietary and confidential.
- */
 
 package org.s4s0l.betelgeuse.utils
 
--- akka-service-commons/src/test/scala/org/s4s0l/betelgeuse/akkacommons/streams/PreciseThrottlerTest.scala
+++ akka-service-commons/src/test/scala/org/s4s0l/betelgeuse/akkacommons/streams/PreciseThrottlerTest.scala
@@ -1,8 +1,3 @@
-/*
- * Copyright© 2018 by Ravenetics Sp. z o.o. - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * This file is proprietary and confidential.
- */
 
 /*
  * Copyright© 2018 the original author or authors.
